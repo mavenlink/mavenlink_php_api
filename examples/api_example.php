e139310f81d2647f99fb6cbbc7a5445c48b15600
Andrew Cantino <cantino@gmail.com>
2014-01-31 18:16:37 -0800

    fix example

--- examples/api_example.php
+++ examples/api_example.php
@@ -1,8 +1,6 @@
 <?php
-
-require 'lib/mavenlink_api.php';
-
-$mavenlinkApi = new MavenlinkApi(<YOUR USER ID>, '<YOUR API KEY>');
+require '../lib/mavenlink_api.php';
+$mavenlinkApi = new MavenlinkApi('<oauthTokenHere>');
 
 $workspacesJson = $mavenlinkApi->getWorkspaces();
 $workspacesDecodedJson = json_decode($workspacesJson, true);
@@ -10,12 +8,12 @@ echo '<h1> Your workspaces: </h1>';
 echo '<ul>';
 $totalBudget = 0;
 
-foreach ($workspacesDecodedJson as $workspace) {
-        echo '<li>' . $workspace[title] . '<br /> Budget: ' . $workspace[price] . '<br /><br /></li>';
-        $totalBudget = $totalBudget + $workspace[price_in_cents];
+foreach ($workspacesDecodedJson[results] as $dataItem) {
+  $workspace = $workspacesDecodedJson[$dataItem[key]][$dataItem[id]];
+  echo '<li>' . $workspace[title] . '<br /> Budget: ' . $workspace[price] . '<br /><br /></li>';
+  $totalBudget = $totalBudget + $workspace[price_in_cents];
 }
 echo '</ul>';
 setlocale(LC_MONETARY, 'en_US');
 echo '<br /><br /><h3> Total Project Budgets: ' . money_format('%i', $totalBudget/100) . '</h3>';
-
 ?>
